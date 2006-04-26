83a0d57fe00dc186ccffe87176aea09d16458a37
Marc Haber <zugschlus@debian.org>
2006-04-26 14:40:25 +0000

    * add patch from Stephen Gran to allow additional groups to be used     for a new user. (mh)     Closes: #147518, #166718, #212452, #233894, #239006, #240707, #240855

--- AdduserCommon.pm
+++ AdduserCommon.pm
@@ -199,6 +199,8 @@ sub preseed_config {
   $configref->{"name_regex"} = "^[a-z][-a-z0-9]*\$";
   $configref->{"exclude_fstypes"} = "(proc|sysfs|usbfs|devpts|tmpfs)";
   $configref->{"skel_ignore_regex"} = "dpkg-(old|new|dist)\$";
+  $configref->{"extra_groups"} = "dialout cdrom floppy audio src video lp users";
+  $configref->{"add_extra_groups"} = 0;
 
   foreach( @$conflistref ) {
       read_config($_,$configref);
