899d7e1e87d4e4e7a61a12b68350f16cd4e4cb93
Marc Haber <zugschlus@debian.org>
2007-01-17 14:08:32 +0000

    improve test9

--- testsuite/test9.pl
+++ testsuite/test9.pl
@@ -6,16 +6,21 @@ use strict;
 use lib_test;
 
 my $error;
+my $output;
 my $groupname = find_unused_groupname();
 my $cmd = "addgroup $groupname";
 
 if (!defined (getgrnam($groupname))) {
-	print "Testing $cmd... ";
-	`$cmd`;
-	my $error = ($?>>8);
+	print "Testing (9.1) $cmd... ";
+	$output=`$cmd 2>&1`;
+	$error = ($?>>8);
 	if ($error) {
 	  print "failed\n  $cmd returned an errorcode != 0 ($error)\n";
 	  exit $error;
+	}
+        if ($output !~ /^Adding group `addusertest\d+' \(GID \d+\) ...\nDone\.\n$/) {
+          print "failed\n  $cmd returned unexpected output ($output)\n";
+	  exit 1;
 	}
 	assert(check_group_exist ($groupname));
 
@@ -24,11 +29,15 @@ if (!defined (getgrnam($groupname))) {
 
 # now testing whether adding the group again fails as it should
 
-print "Testing $cmd... ";
-`$cmd`;
+print "Testing (9.2) $cmd... ";
+$output=`$cmd 2>&1`;
 $error = ($?>>8);
 if ($error ne 1) {
   print "failed\n  $cmd returned an errorcode != 1 ($error)\n";
+  exit 1;
+}
+if ($output !~ /^addgroup: The group `addusertest\d+' already exists\.\n$/ ) {
+  print "failed\n  $cmd returned unexpected output ($output)\n";
   exit 1;
 }
 print "ok\n";
@@ -37,12 +46,16 @@ print "ok\n";
 # fails as it should (#405905)
 
 $cmd = "addgroup --system $groupname";
-print "Testing $cmd... ";
-`$cmd`;
+print "Testing (9.3) $cmd... ";
+$output=`$cmd 2>&1`;
 $error = ($?>>8);
 if ($error ne 1) {
   print "failed\n  $cmd returned an errorcode != 1 ($error)\n";
   exit $error;
+}
+if ($output !~ /^addgroup: The group `addusertest\d+' already exists and is not a system group.$/ ) {
+  print "failed\n  $cmd returned unexpected output ($output)\n";
+  exit 1;
 }
 print "ok\n";
 
@@ -50,12 +63,16 @@ my $sysgroupname = find_unused_groupname();
 $cmd = "addgroup --system $sysgroupname";
 
 if (!defined (getgrnam($sysgroupname))) {
-	print "Testing $cmd... ";
-	`$cmd`;
+	print "Testing (9.4) $cmd... ";
+	$output=`$cmd 2>&1`;
 	$error = ($?>>8);
 	if ($error) {
 	  print "failed\n  $cmd returned an errorcode != 0 ($error)\n";
 	  exit $error;
+	}
+        if ($output !~ /^Adding group `addusertest\d+' \(GID \d+\) ...\nDone\.\n$/ ) {
+	  print "failed\n  $cmd returned unexpected output ($output)\n";
+	  exit 1;
 	}
 	assert(check_group_exist ($sysgroupname));
 
@@ -68,12 +85,16 @@ if (!defined (getgrnam($sysgroupname))) {
 my $gid = getgrnam($sysgroupname);
 
 $cmd = "addgroup --system $sysgroupname --gid $gid" ;
-print "Testing $cmd... ";
-`$cmd`;
+print "Testing (9.5) $cmd... ";
+$output=`$cmd 2>&1`;
 $error = ($?>>8);
 if ($error) {
   print "failed\n  $cmd returned an errorcode != 0 ($error)\n";
   exit $error;
+}
+if ($output !~ /^The group `addusertest\d+' already exists as a system group\. Exiting\.\n$/ ) {
+  print "failed\n  $cmd returned unexpected output ($output)\n";
+  exit 1;
 }
 print "ok\n";
 
@@ -81,11 +102,15 @@ print "ok\n";
 # fails as it should
 
 $cmd = "addgroup $sysgroupname";
-print "Testing $cmd... ";
-`$cmd`;
+print "Testing (9.6) $cmd... ";
+$output=`$cmd 2>&1`;
 $error = ($?>>8);
 if ($error ne 1) {
   print "failed\n  $cmd returned an errorcode != 1 ($error)\n";
+  exit 1;
+}
+if ($output !~ /^addgroup: The group `addusertest\d+' already exists\.$/ ) {
+  print "failed\n  $cmd returned unexpected output ($output)\n";
   exit 1;
 }
 print "ok\n";
